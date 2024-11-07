8fae7ba05cae59be92a55d223af74754617f5718
reggyball <rtcaraan@up.edu.ph>
2024-11-07 14:34:36 +0800

    changes practice problems

--- desktop/test2.py
+++ desktop/test2.py
@@ -195,11 +195,58 @@
 # max = int(input("Enter n: "))
 # print(fibonacci(max=max))
 
-def fib(n):
-    if n == 1 or n == 0:
-        return n
+# def fib(n):
+#     if n == 1 or n == 0:
+#         return n
     
-    return fib(n-1) + fib(n-2)
+#     return fib(n-1) + fib(n-2)
 
-n = int(input("Enter n: "))
-print(fib(n))
+# n = int(input("Enter n: "))
+# print(fib(n))
+#---------------------------------------------------------------------------------------
+#PRACTICE PROBLEMS
+#Create a program that determines the largest number among the four inputs of the user.
+#determines the largest number among the four inputs of the user.
+
+# prompts = ["first", "second", "third", "fourth",]
+# numbers = [float(input("Enter " + prompt + " number: ")) for prompt in prompts]
+# print("The largerst number is", max(numbers))
+
+
+# create a list called prompts that contains a part of the string that will serve as the custom message in the input query. This also where the for loop inside the comprehension loops through.
+# use a comprehension to create a list that will store the numbers from the user input.
+# Since the comprehension uses the function 'input' to obtain the user input, it needs to be typecasted to float, as the return value of input is a string.
+# use max function to determine the element with the highest value and print the return value
+
+
+#CONDITIONALS
+#Create a program that determines the largest number among the four inputs of the user.
+
+prompts = ["first", "second", "third", "fourth"]
+larger = float('-inf')
+for i in range(4):
+    num = float(input(f"Enter {prompts[i]} number: "))
+    if num > larger:
+        larger = num
+print(larger)
+
+
+# prompts = ["first", "second", "third", "fourth"]
+# larger = float(input(f"Enter first number: "))
+# for i in range(1, 4):
+#     num = float(input(f"Enter {prompts[i]} number: "))
+#     if num > larger:
+#         larger = num
+# print(larger)
+
+# prompts = ["first", "second", "third", "fourth"]
+# larger = None
+# for i in range(4):
+#     num = float(input(f"Enter {prompts[i]} number: "))
+#     if larger == None:
+#         larger = num
+#     if num > larger:
+#         larger = num
+# print(larger)
+
+
