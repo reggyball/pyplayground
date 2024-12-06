207cd98ec05d0c4b3489dfb8f8a0d6cf20d10762
reggyball <rtcaraan@up.edu.ph>
2024-12-06 16:12:42 +0800

    updates

--- desktop/activity7/activity7-caraan-bonus.py
+++ desktop/activity7/activity7-caraan-bonus.py
@@ -1,4 +1,4 @@
-import math
+import math                                                                                                     
 from pathlib import Path
 
 #activity 7 as a function, calculates count, term frequency of each term in document
@@ -22,13 +22,12 @@ def termfreq(filename):
                     if letter in elphaba_list: 
                         term += letter
 
-            if term in stopwords_list:
-                continue
-            else:
-                    key_list.append(term)
+            if term and term not in stopwords_list:
+                key_list.append(term)
+                    
+    term_freq = {}
 
     nd = len(key_list)
-    term_freq = {}
 
     while len(key_list) > 0:
         term = key_list[0]
@@ -43,38 +42,49 @@ def termfreq(filename):
     return term_freq
 
 
-#Access directory of provided path
-pathdir = input("Enter path directory - Format: ./path/ (if unk, enter for current dir): ")
+#To access directory of provided path
+pathdir = input("Enter path directory (if unk, enter for default: current dir): ")
+if '"' in pathdir:                                                                               # Remove double quotes from the provided directory path, if present
+    pathdir = pathdir.replace('"', '')
+if pathdir and pathdir[-1] != "/":                                                               # Ensure the path ends with a forward slash, if not already provided 
+    pathdir += "/"
 
-#defaults pathdirectory as to current if no path was provided by the user
-if pathdir == "":
-    current_dir = Path.cwd()
-    directory = Path(current_dir)
-else:
-    directory = Path(pathdir)
+#Defaults pathdirectory as to current dir if no path was provided by the user
+directory = Path(pathdir) if pathdir else Path.cwd()
 
 #Obtain all filenames of text files inside the directory
 file_list = [str(file).split("\\")[-1] for file in directory.glob("*.txt")]
 
+
 tf_idf_dict = {}
 
+#All filenames in file_list be extracted as key for the dictionary, values will be the counts returned by termfreq function
 for filename in file_list: 
-    tf_idf_dict[filename] = termfreq(pathdir+filename)
+    tf_idf_dict[filename] = termfreq(pathdir+filename)                                              #argument used is (pathdir + filename) to provide direction on where to look for the filename
 
-# print(tf_idf_dict)
-
+#Compute IDF for each term
 #idf = log(total number of documents/number of documents with term t in it)
-a = len(file_list)
 
-term_keys = []
-for key in tf_idf_dict.keys():
-    term_keys += list(tf_idf_dict[key].keys())
+a = len(file_list)                                                                                  #a or the total number of documents
 
-for key in tf_idf_dict.keys():
-    for term in tf_idf_dict[key].keys():
-        b = term_keys.count(term)
-        idf = math.log(a/b)
-        tf_idf_dict[key][term]['idf'] = idf
+term_keys = []                                                                                  #To count the number of documents mentioning the term:
+for key in tf_idf_dict.keys():                                                                      #For each document, all the terms (keys of the term-counts dict) are extracted and added to the term_keys list.
+    term_keys += list(tf_idf_dict[key].keys())                                                      #Since term_keys collects terms from all documents, it contains duplicates whenever a term appears in multiple documents.
+                                                                                                    #eg: The term appears in term_keys once for every document it is mentioned in.
+                                                                                                    #hence, the no. of times the term appears in the term_keys is equivalent to the no. of documents term was mentioned. 
 
-# print("")
-print(tf_idf_dict)
+for key in tf_idf_dict.keys():                                                                          
+    for term in tf_idf_dict[key].keys():                                                            #no. occurence of term in the list is equal to b
+        b = term_keys.count(term)                                                                   #b or the number of documents with the term 
+        idf = math.log(a/b)                                                                         #compute for idf; natural logarithim (log base e is used in formula calculation vs common log(logbase10)) is used in the formula
+        tf_idf_dict[key][term]['idf'] = idf                                                         #update the term values to include the idf
+
+
+
+with open("TF-IDF.txt", "w") as file:                                                               #open text file in write mode/create if not yet available
+    for keyword, term_freq in tf_idf_dict.items():                                                  #write the contents of the dictionary in the text file given the following format
+        file.write(f"'{keyword}':  \n" )                                                            #writes document name                                    
+        for term, value in term_freq.items():                                                                                               
+            file.write(f"\t'{term}' : {value}\n")                                                   #writes term and counts 
+        
+
--- desktop/activity7/activity7-caraan.py
+++ desktop/activity7/activity7-caraan.py
@@ -9,30 +9,29 @@ elphaba_list = "abcdefghijklmnopqrstuvwxyz"
 with open ("stop_words.txt", "r", encoding="utf-8") as file:                                #Text file is opened at read mode with UTF-8 encoding to ensure program reads text files with any character                              
     for line in file:                                                                       #text file is read per line
         stop_word = line.strip()                                                            #each word per line is extracted as a 'stop_word' by removing any trailing spaces
-        stopwords_list.append(stop_word)                                                    #each stop word is appended in the empty stop_words list
+        stopwords_list.append(stop_word)                                                    #each stop word is appended in the stopwords_list
                                                                                             #when finished processing, text file is automatically closed due to "with open" syntax
 
 #content of article.txt is processed and appended to key_list
-with open("article.txt", "r") as file:                                                      #Text file is opended at read mode, automatically closed after use
+with open("article.txt", "r") as file:                                                      #Text file is opened at read mode, automatically closed after use
     for line in file:                                                                       #Processing will be per each line:
        for word in line.split():                                                               #To extract words per line, split at spaces
            word = word.lower().strip()                                                         #Each word will be in lowercase and removed of trailing spaces
 
 #to remove special characters and punctuations in word
            term = ""                                                                           #declare an empty string "term"
-           for letter in word:                                                                 #Each letter of the word will be assesed if in alphabet list, 
-                if letter in elphaba_list:                                                            #if yes each letter will be concatenated to 'term' string 
+           for letter in word:                                                                 #Each letter of the word will be assesed if in the alphabet list, 
+                if letter in elphaba_list:                                                        #if yes each letter will be concatenated to 'term' string
                     term += letter                                                                      
 
-#check if term is in stop_words                                                                                                       
-           if term in stopwords_list:                                                          #if term is not in stop_words list then it will be appended to the key_list
-               continue                                                                        #key_list will contain all the valid terms in the document (words less the punctuation and special chars and not in stop words)
-           else:
-                key_list.append(term)
+#check if term is empty and in stop_words                                                                                                       
+           if term and term not in stopwords_list:                                                #if term is not empty and not in stop_words list then it will be appended to the key_list
+                key_list.append(term)                                                             #key_list will contain all the valid terms in the document (words less the punctuation and special chars, and not in stop words)
+                
 
 #term_freq(nt, nd) = no of times term appeared in doc/no of terms in doc
 
-#declare the dictionary
+#ASSEMBLING THE DICTIONARY
 term_freq = {}
 
 nd = len(key_list)                                                                            #nd = no of terms in the document is the number of elements in the key_list
@@ -49,11 +48,12 @@ while len(key_list) > 0:                                                        
 
 term_freq = dict(sorted(term_freq.items()))                                                     #sort the dictionary alphabetically
 
-
 #copy created dictionary to term_freq text
 with open("term_frequency.txt", "w") as file:                                                   #open text file in write mode/create if not yet available
     for keyword, value in term_freq.items():                                                    #for each key:val pair in dictionary,
         file.write(f"'{keyword}' : {value}\n")                                                      #copy to the text file given the format
+
+
 
 
         
